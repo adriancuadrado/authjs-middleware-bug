0fff73da04738a2aeaa933dd05bf8cdb1fcc8162
adriancuadrado <29214635+adriancuadrado@users.noreply.github.com>
2025-06-23 06:20:31 +0200

    What's the purpose of being able to pass a custom ballback to the middleware?

--- src/auth.ts
+++ src/auth.ts
@@ -2,12 +2,11 @@ import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
-  // It doesn't matter if you have this "callbacks" property commented or not, in either case `middleware.ts` won't run.
-  //
-  // callbacks: {
-  //   authorized: async () => {
-  //     console.log("Hello world from auth.ts");
-  //     return true;
-  //   },
-  // },
+  callbacks: {
+    authorized: async ({ request }) => {
+      console.log("Hello world from auth.ts");
+      console.log("Request url: " + request.url);
+      return true;
+    },
+  },
 });
